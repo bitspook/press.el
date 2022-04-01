fe4a446d4dd1f729a177c7ef408c616a2074409e
Charanjit Singh <bitspook@protonmail.com>
2022-04-01 21:53:28 +0530

    fix: some style fixes

--- publish.el
+++ publish.el
@@ -4,11 +4,11 @@
 (require 'org-roam)
 (require 'cl-lib)
 
-(defvar spub--staging-dir (expand-file-name "./_staged"))
-(defvar spub--publish-dir (expand-file-name "./_published"))
-(defvar spub--legacy-content-dir (expand-file-name "./content"))
-(defvar spub--static-dir (expand-file-name "./static"))
-(defvar spub--index-file-name "index.org")
+(defvar spub--staging-dir (expand-file-name "./_staged/"))
+(defvar spub--publish-dir (expand-file-name "./_published/"))
+(defvar spub--legacy-content-dir (expand-file-name "./content/"))
+(defvar spub--static-dir (expand-file-name "./static/"))
+(defvar spub--index-file (expand-file-name "index.org"))
 
 (defun spub--roam-nodes-with-tags (tags)
   "Find all org-roam nodes which have all TAGS."
@@ -25,12 +25,12 @@ in the staging area before handing it over to `org-mode' for
 publishing as a single project."
   (let* ((staging-dir spub--staging-dir)
          (legacy-content-dir spub--legacy-content-dir)
-         (index-file spub--index-file-name)
+         (index-file spub--index-file)
          (blog-posts-to-publish (spub--roam-nodes-with-tags '("blog-post" "published"))))
     (spub--clean-stage staging-dir)
 
     (copy-directory legacy-content-dir staging-dir nil t t)
-    (copy-file index-file (expand-file-name index-file staging-dir) t)
+    (copy-file index-file staging-dir t)
 
     (cl-dolist (node blog-posts-to-publish)
       (copy-file (org-roam-node-file node)
@@ -48,7 +48,7 @@ publishing as a single project."
                   :auto-preamble nil
                   :with-toc nil
                   :with-drawers nil
-                  :auto-sitemap nil
+                  :auto-sitemap t
                   :html-style nil))
          (static `("static"
                    :base-directory ,spub--static-dir
