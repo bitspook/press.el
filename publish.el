0b6b0a5cc376a6e5ae3eaf325340597097c85496
Charanjit Singh <bitspook@protonmail.com>
2022-04-04 21:00:20 +0530

    feat: a top menu

--- publish.el
+++ publish.el
@@ -9,6 +9,7 @@
 (defvar spub--legacy-content-dir (expand-file-name "./content/"))
 (defvar spub--static-dir (expand-file-name "./static/"))
 (defvar spub--index-file (expand-file-name "index.org"))
+(defvar spub--preamble-file (expand-file-name "./preamble.html"))
 
 (defun spub--roam-nodes-with-tags (tags)
   "Find all org-roam nodes which have all TAGS."
@@ -16,6 +17,11 @@
                 (lambda (node) (= (seq-length tags) (seq-length (seq-intersection (org-roam-node-tags node) tags))))
                 (org-roam-node-list))))
     nodes))
+
+(defun spub--clean ()
+  "Cleanup intermediate and published content."
+  (delete-directory spub--staging-dir t)
+  (delete-directory spub--publish-dir t))
 
 (defun spub--stage ()
   "Prepare the staging area for publishing.
@@ -27,7 +33,7 @@ publishing as a single project."
          (legacy-content-dir spub--legacy-content-dir)
          (index-file spub--index-file)
          (blog-posts-to-publish (spub--roam-nodes-with-tags '("blog-post" "published"))))
-    (spub--clean-stage staging-dir)
+    (spub--clean)
 
     (copy-directory legacy-content-dir staging-dir nil t t)
     (copy-file index-file staging-dir t)
@@ -36,10 +42,15 @@ publishing as a single project."
       (copy-file (org-roam-node-file node)
                  (expand-file-name "./blog/" staging-dir)))))
 
-(defun spub--publish ()
+(defun spub--publish (&optional force? async? sitemap?)
   "Publish the project."
   (defvar org-publish-project-alist)
-  (let* ((posts `("posts"
+  (let* ((user-full-name "Charanjit Singh")
+         (org-html-preamble t)
+         (org-html-preamble-format `(("en" ,(with-temp-buffer
+                                              (insert-file-contents spub--preamble-file)
+                                              (buffer-string)))))
+         (posts `("posts"
                   :base-directory ,spub--staging-dir
                   :recursive t
                   :base-exteinsion "org"
@@ -47,8 +58,11 @@ publishing as a single project."
                   :publishing-function org-html-publish-to-html
                   :auto-preamble nil
                   :with-toc nil
+                  :with-creator nil
                   :with-drawers nil
-                  :auto-sitemap t
+                  :sitemap-filename "index.org"
+                  :auto-sitemap ,sitemap?
+                  :template "./templates/test.org"
                   :html-style nil))
          (static `("static"
                    :base-directory ,spub--static-dir
@@ -59,8 +73,16 @@ publishing as a single project."
          (project `("project" :components ("static" "posts")))
          (org-publish-project-alist (list posts project static))
          (org-html-head-include-default-style nil)
-         (org-html-head "<link rel=\"stylesheet\" href=\"/dist/main.css\"></link>")
+         (org-html-head
+          (string-join
+           '("<link rel=\"stylesheet\" href=\"/dist/main.css\"></link>"
+             "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">")
+           "\n"))
          (js-mode-hook nil))
-    (spub--clean)
+    (when force? (spub--clean))
     (spub--stage)
-    (org-publish-project (car project) t)))
+    (org-publish-project (car project) force? async?)))
+
+(spub--publish)
+
+;;; publish.el ends here
